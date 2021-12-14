9bd541e3f60602337ff3a7ef47294837f192e7f0
Aleksander Jess <jess.aleksander@gmail.com>
2021-12-14 21:43:15 +0100

    wrapping the app with an InheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import './views/plan_screen.dart';
+import "./plan_provider.dart";
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.purple),
-      home: const PlanScreen(),
+      home: PlanProvider(child: const PlanScreen()),
     );
   }
 }
